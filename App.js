cdd6944353c6525c3d0aa3fbcca6176fdd6d7dfb
Daniel-Santin <32037139@mackenzista.com.br>
2022-10-23 22:58:59 -0300

    ca

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import { StatusBar } from 'react-native';
-import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Routes from './pages/routes';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
    <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
     <StatusBar backgroundColor={'#7f7f7f'} barStyle="light-content"/>
-    <Routes/>
    </NavigationContainer>
   );
 }
