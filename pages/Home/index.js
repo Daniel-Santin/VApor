2250bb984fd24ae4dab24d2b9bac76a8946edf16
Daniel-Santin <32037139@mackenzista.com.br>
2022-10-23 21:49:29 -0300

    pinto

---
+++ pages/Home/index.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { View,Text,StyleSheet,Image,TextInput,TouchableOpacity } from "react-native";
+import * as Animatable from 'react-native-animatable'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export default function Home( ){
+    return(
+        <View style={styles.container}>
+            <Animatable.View animation="fadeInUp"style={styles.containerForm}>
+                <Text style={styles.Title}>
+                Apressadinho
+                </Text>
+                <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('QuemSomos')}>
+                    <Text>
+                        Entrar
+                    </Text>
+                </TouchableOpacity>
+            </Animatable.View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{ flex: 1,
+        alignItems:'center',
+        justifyContent:'center',
+        backgroundColor: '#ADD8E6',
+    },
+    containerLogo:{
+        flex: 1,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    containerForm:{
+        flex:1,
+        borderTopLeftRadius: 25,
+        borderTopRightRadius: 25,
+        borderBottomRightRadius: 25,
+        borderBottomLeftRadius: 25,
+        padding: '5%',
+        marginBottom: 250,
+        backgroundColor:'#ADD8E6'
+    },
+    Title:{
+        marginTop:100,
+        fontWeight:"bold",
+        color:'#111',
+        fontSize: 54
+    },
+    Text:{
+        fontFamily:'SansSeriff',
+        color:'#888'
+    },
+    button:{
+        position:'absolute',
+        backgroundColor:'#fff',
+        borderRadius:50,
+        paddingVertical: 8,
+        width:'30%',
+        alignSelf:'center',
+        bottom:'15%',
+        alignItems:'center',
+        justifyContent:'center'
+    },
+    buttonText:{
+        fontSize:18,
+        color:'#ccc'
+    }
+})
--- pages/routes/index.js
+++ pages/routes/index.js
@@ -2,13 +2,14 @@ import * as React from 'react';
 import { StatusBar } from 'react-native';
 import { NavigationContainer, StackActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Routes from './pages/routes';
 
-export default function App() {
-  return (
-   <NavigationContainer>
-    <StatusBar backgroundColor={'#7f7f7f'} barStyle="light-content"/>
-    <Routes/>
-   </NavigationContainer>
-  );
-}
+export function Routes() {
+    return (
+        <Tab.Navigator>
+          <Tab.Screen 
+            name="Home" 
+            component={Home} 
+            options={{ headerShown: false }}/>
+        </Tab.Navigator>
+    );
+  }
