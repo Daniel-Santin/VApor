881ac28fdc4b093383cff7c194c3de5577a43d1f
Gustavo Duarte ALves <gualves2001@icloud.com>
2022-12-05 09:31:28 -0300

    ci: add first developmening, 6 months

--- pages/Desenvolvimento/index.js
+++ pages/Desenvolvimento/index.js
@@ -1,13 +1,38 @@
-import { ScrollView } from "react-native";
+import { ScrollView, StyleSheet, Pressable } from "react-native";
 import Slider from "../../components/Slider";
 import Text from "../../components/Text";
 import { colors } from "../../themes/color";
 
-const Root = () => (
+import { useNavigation } from "@react-navigation/native";
+
+const Root = () => {
+    const navigation = useNavigation();
+    return (
     <ScrollView>
-        <Text align="center" color={colors.pink}> SELECIONE QUE IDADE QUER PESQUISAR: </Text>
+        <Text align="center" color="#7d46de"> SELECIONE QUE IDADE QUER PESQUISAR: </Text>
+
+        <Pressable
+                    style={styles.button}
+                    onPress={() => navigation.navigate('2 meses')}
+                >
+                    <Text color="#fff" style={styles.buttonText}> 2 Meses </Text>
+                </Pressable>
+
+                <Pressable
+                    style={styles.button}
+                    onPress={() => navigation.navigate('4 meses')}
+                >
+                    <Text color="#fff" style={styles.buttonText}> 4 Meses </Text>
+                </Pressable>
+
+                <Pressable
+                    style={styles.button}
+                    onPress={() => navigation.navigate('6 meses')}
+                >
+                    <Text color="#fff" style={styles.buttonText}> 6 Meses </Text>
+                </Pressable>
     </ScrollView>
-)
+)}
 
 const MarcosDosDesenvolvimentos = () => (
     <Slider title="OS MARCOS DO DESENVOLVIMENTO" slides={[
@@ -17,5 +42,46 @@ const MarcosDosDesenvolvimentos = () => (
         }
     ]} />
 )
+
+const styles = StyleSheet.create({
+    container:{ 
+        display: 'flex',
+        justifyContent:'space-around',
+        alignItems: 'center',
+        backgroundColor: '#7d46de',
+        height: '100%'
+    },
+    content: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        width: '100%',
+        minHeight: '60%'
+    },
+    title:{
+        fontWeight:"bold",
+        color:'#f8f8f8',
+        fontSize: 54
+    },
+    button:{
+        padding: 16,
+        backgroundColor: '#7d46de',
+        borderRadius: 8,
+        width: '80%',
+        fontSize: 24,
+        display: 'flex',
+        alignItems: 'center',
+        alignSelf: 'center',
+        marginTop: 12
+    },
+    buttonText: {
+        color: '#fff',
+    },
+    logo: {
+        width: 300,
+        height: 300,
+    }
+})
+
 
 export default MarcosDosDesenvolvimentos;
